29ccf5e53961a88abef3b4c859fe9f457f4f26ce
VerasNp <vitorveras@proton.me>
2026-05-07 17:40:38 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ratfy backend
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express, { Router, type Request, type Response } from "express";
+
+const app = express();
+const port = process.env.PORT ?? "9001";
+
+const route = Router();
+
+app.use(express.json());
+
+route.get("/", (req: Request, res: Response) => {
+	res.json({ message: "hello world with Tyspescript" });
+});
+
+app.use(route);
+
+app.listen(port, () => `server running on port ${port}`);
