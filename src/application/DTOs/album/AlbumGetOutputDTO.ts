7860bf3788e68d79501f973e8cdef0ee454c383c
itznokx <dimitri.b.medeiros@gmail.com>
2026-06-14 23:02:25 -0300

    fix(album dto): create dto input and output for every use case

---
+++ src/application/DTOs/album/AlbumGetOutputDTO.ts
@@ -0,0 +1,14 @@
+export type AlbumGetOutputDTO = {
+  albumType:        string
+  artistIds:        string[]
+  createdAt:        Date
+  id:               string
+  isDeleted:        boolean
+  isPublic:         boolean
+  label:            string
+  name:             string
+  releaseDate:      string
+  releasePrecision: string
+  totalTracks:      number
+  updatedAt:        Date
+}
---
+++ src/application/DTOs/album/AlbumListInputDTO.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+
+export const AlbumListSchema = z.object({
+  limit: z.coerce.number().int().min(1).max(50).default(20),
+  page:  z.coerce.number().int().min(1).default(1),
+})
+
+export type AlbumListInputDTO = z.infer<typeof AlbumListSchema>
