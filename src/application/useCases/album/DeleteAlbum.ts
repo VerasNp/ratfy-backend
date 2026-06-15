936dfb52b031dcccd2d81ff5870c429841416613
itznokx <dimitri.b.medeiros@gmail.com>
2026-06-14 23:25:04 -0300

    fix(delete use case): use input and output dtos

--- src/application/useCases/album/DeleteAlbum.ts
+++ src/application/useCases/album/DeleteAlbum.ts
@@ -1,9 +1,11 @@
+import type { AlbumDeleteInputDTO } from '#application/DTOs/album/AlbumDeleteInputDTO.js'
+import type { AlbumDeleteOutputDTO } from '#application/DTOs/album/AlbumDeleteOutputDTO.js'
 import type { AlbumRepository } from '#application/ports/AlbumRepository.js'
 
 export class DeleteAlbumUseCase {
   constructor(private readonly albumRepo: AlbumRepository) {}
 
-  async execute(id: string): Promise<void> {
-    await this.albumRepo.delete(id)
+  async execute(dto: AlbumDeleteInputDTO): Promise<AlbumDeleteOutputDTO> {
+    await this.albumRepo.delete(dto.id)
   }
 }
