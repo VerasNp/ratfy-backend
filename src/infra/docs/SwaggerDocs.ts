6eceeeb614984d004bfa2476ea22996568415d35
Andṟé Willyan <andrewillyan@tutanota.com>
2026-07-02 15:50:51 -0300

    docs: adiciona documentação do endpoint de favoritos

--- src/infra/docs/SwaggerDocs.ts
+++ src/infra/docs/SwaggerDocs.ts
@@ -7,6 +7,7 @@ import PlaylistControllerSwagger from './routes/playlist/PlaylistController.swag
 import UserControllerSwagger from './routes/user/UserController.swagger'
 import VerifyEmailControllerSwagger from './routes/email/VerifyEmailController.swagger'
 import AlbumControllerSwagger from './routes/album/AlbumController.swagger'
+import FavoriteControllerSwagger from './routes/favorite/FavoriteController.swagger'
 import TrackControllerSwagger from './routes/track/TrackController.swagger'
 
 class SwaggerDocs implements DocsPort {
@@ -41,6 +42,7 @@ class SwaggerDocs implements DocsPort {
 		new AuthControllerSwagger(this.registry)
 		new PlaylistControllerSwagger(this.registry)
 		new AlbumControllerSwagger(this.registry)
+		new FavoriteControllerSwagger(this.registry)
 		new UserControllerSwagger(this.registry)
 		new VerifyEmailControllerSwagger(this.registry)
 		new TrackControllerSwagger(this.registry)
